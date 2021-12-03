a81ba78e0f2543b40a6cec6dfdc077f698e04ff8
MikeDu <mdd.dev@live.com>
2021-12-03 09:21:06 -0500

    First  Version

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+- 👋 Hi, I'm Mike Dumais
+
+- 👀 I’m interested in GIS & web development
+
+- 🌱 I’m currently learning backend development
+
+- 📫 Email: mdd.dev@live.com
+
+- 🐦 Twitter: @theMikeyMan
+
+`)

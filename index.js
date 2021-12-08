ac20a97e637dcf31cf74ebe8c14b5ba04f48a890
mdd66 <mdd@live.com>
2021-12-07 19:43:39 -0500

    Added gitignore

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+    title: pkgJSON.name,
+    tagLine: `✋Hi, nice to meet yah!`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true, 
+});
+
 console.log(`
-- 👋 Hi, I'm Mike Dumais
+- 👋 I'm Mike Dumais
 
-- 👀 I’m interested in GIS & web development
+- 👀 I’m interested in GIS & modern web development
 
-- 🌱 I’m currently learning backend development
+- 🌱 I’m currently learning Node & backend development
 
 - 📫 Email: mdd.dev@live.com
 
-- 🐦 Twitter: @theMikeyMan
+- 🐦 Twitter: @_MikeDu
 
 `)
